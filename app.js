6ba124573cd83ea19d685d89b0f178e37f11e18f
vghetta <valerio.ghetta@allitude.it>
2025-03-24 11:57:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+	res.send('Hello, Corso DO101 pippo!\n');
+});
+app.listen(8080, function () {
+	console.log('Example app listening on port 8080!');
+});
+module.exports = app;
