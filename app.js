0fdc1b2236976e4f28650126eb21cdf6129078f5
Student User <student@workstation.lab.example.com>
2025-03-24 07:19:17 -0400

    modifica

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-	res.send('Hello, Corso DO101 pippo!\n');
+	res.send('Hello, Corso DO101 !\n');
 });
 app.listen(8080, function () {
 	console.log('Example app listening on port 8080!');
